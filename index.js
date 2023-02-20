81fc30edf217e196aa7b34b081abf149b3ddded0
João Vitor <jv.zaniolo@gmail.com>
2023-02-20 14:34:18 -0300

    Remove prejss-cli in favor of postcss-js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+function cssToJS(file) {
+  const css = require('fs').readFileSync(
+    require('path').resolve(__dirname, file),
+    'utf8',
+  );
+  const root = require('postcss').parse(css);
+  return require('postcss-js').objectify(root);
+}
+
 module.exports = require('tailwindcss/plugin')(
   function ({ addBase, addComponents }) {
-    addBase(require('./dist/base'));
-    addComponents(require('./dist/components'));
+    addBase(cssToJS('./dist/base.css'));
+    addComponents(cssToJS('./dist/components.css'));
   },
   {
     theme: {
