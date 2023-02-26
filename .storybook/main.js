adf7e6603a504a88326fec19cfad927041af2889
João Vitor <jv.zaniolo@gmail.com>
2023-02-25 22:33:43 -0300

    fix(package.json): update dependencies [skip ci]

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,6 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/addon-styling',
   ],
   staticDirs: ['../node_modules/@fontsource/montserrat'],
   framework: {
