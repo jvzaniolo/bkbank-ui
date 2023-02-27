b8f9ca9a39d7f91be6cc58952eaa5061a4238c5b
João Vitor <joao.oliveira@bkbank.com.br>
2023-02-27 15:54:15 -0300

    feat(badge): migrate to typescript

--- storybook/stories/components/badge/badge.stories.ts
+++ storybook/stories/components/badge/badge.stories.ts
@@ -13,7 +13,7 @@ const meta = {
   },
   args: {
     base: 'badge',
-    color: 'badge-highlight',
+    color: undefined,
     content: '32',
   },
   argTypes: {
@@ -24,12 +24,8 @@ const meta = {
     },
     color: {
       description: 'Available colors for the badge.',
-      table: { defaultValue: { summary: 'Unstyled' } },
       options: [undefined, 'badge-highlight'],
-      control: {
-        type: 'radio',
-        labels: { undefined: 'Unstyled', 'badge-highlight': 'Highlight' },
-      },
+      control: { type: 'radio', labels: { undefined: 'unstyled' } },
     },
     content: {
       description: 'Change the content of the badge in the example.',
@@ -41,21 +37,9 @@ const meta = {
 export default meta;
 
 export const Default: StoryObj = {
+  args: {
+    color: 'badge-highlight',
+  },
   render: args =>
     `<div class="${clsx(args.base, args.color)}">${args.content}</div>`,
-};
-
-/**
- * Available colors for the badge component.
- *
- * Unstyled: `@default`
- * Highlight: `badge-highlight`
- */
-export const Colors: StoryObj = {
-  render: args => `
-    <div class="flex gap-4">
-      <div class="${clsx(args.base)}">${args.content}</div>
-      <div class="${clsx(args.base, 'badge-highlight')}">${args.content}</div>
-    </div>
-  `,
 };
