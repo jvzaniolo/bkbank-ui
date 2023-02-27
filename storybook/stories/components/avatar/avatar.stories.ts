6ff00189dfe622814c0c7e8917cd559d136252dc
João Vitor <joao.oliveira@bkbank.com.br>
2023-02-27 15:54:03 -0300

    feat(avatar): migrate to typescript

--- storybook/stories/components/avatar/avatar.stories.ts
+++ storybook/stories/components/avatar/avatar.stories.ts
@@ -17,6 +17,7 @@ const meta = {
     base: 'avatar',
     size: undefined,
     color: undefined,
+    state: undefined,
     content: 'text',
   },
   argTypes: {
@@ -27,29 +28,21 @@ const meta = {
     },
     color: {
       description: 'Available colors for the avatar component.',
-      table: { defaultValue: { summary: 'Unstyled' } },
+      table: { defaultValue: { summary: 'unstyled' } },
       options: [undefined, 'avatar-high', 'avatar-primary'],
-      control: {
-        type: 'radio',
-        labels: {
-          undefined: 'Unstyled',
-          'avatar-high': 'High',
-          'avatar-primary': 'Primary',
-        },
-      },
+      control: { type: 'radio', labels: { undefined: 'unstyled' } },
     },
     size: {
-      description: 'Available sizes for the avatar component.',
-      table: { defaultValue: { summary: 'Small' } },
+      description: 'Available sizes for the avatar component',
+      table: { defaultValue: { summary: 'unstyled' } },
       options: [undefined, 'avatar-md', 'avatar-lg'],
-      control: {
-        type: 'radio',
-        labels: {
-          undefined: 'Small',
-          'avatar-md': 'Medium',
-          'avatar-lg': 'Large',
-        },
-      },
+      control: { type: 'radio', labels: { undefined: 'unstyled' } },
+    },
+    state: {
+      description: 'Possible states for the avatar component.',
+      table: { defaultValue: { summary: 'unstyled' } },
+      options: [undefined, 'avatar-active'],
+      control: { type: 'radio', labels: { undefined: 'unstyled' } },
     },
     content: {
       description:
@@ -70,100 +63,4 @@ export const Default: Story = {
     content: 'text',
     color: 'avatar-primary',
   },
-};
-
-/**
- * Available sizes for the avatar component.
- *
- * Small: `@default`
- * Medium: `avatar-md`
- * Large: `avatar-lg`
- */
-export const Sizes: Story = {
-  args: {
-    base: 'avatar',
-    color: 'avatar-primary',
-    content: 'text',
-  },
-  render: args => `
-    <div class="flex gap-4">
-      ${createAvatar(args)}
-      ${createAvatar({ ...args, size: 'avatar-md' })}
-      ${createAvatar({ ...args, size: 'avatar-lg' })}
-    </div>
-  `,
-};
-
-/**
- * Available colors for the avatar component.
- *
- * Unstyled: `@default`
- * High: `avatar-high`
- * Primary: `avatar-primary`
- */
-export const Colors: Story = {
-  args: {
-    base: 'avatar',
-    content: 'text',
-  },
-  render: args => `
-    <div class="flex gap-4">
-      ${createAvatar(args)}
-      ${createAvatar({ ...args, color: 'avatar-high' })}
-      ${createAvatar({ ...args, color: 'avatar-primary' })}
-    </div>
-  `,
-};
-
-/**
- * Possible content for the avatar component. This is just for demonstration purposes. You don't need to pass any additional class.
- *
- * Text: `string`
- * Icon: `<svg>` or `<i>`
- * Image: `<img>`
- */
-export const Content: Story = {
-  args: {
-    base: 'avatar',
-    color: 'avatar-primary',
-  },
-  render: args => `
-    <div class="flex gap-4">
-      ${createAvatar({ ...args, content: 'text' })}
-      ${createAvatar({ ...args, content: 'icon' })}
-      ${createAvatar({ ...args, content: 'image' })}
-    </div>
-  `,
-};
-/**
- * Possible states for the avatar component.
- *
- * Unstyled: `@default`
- * Active: `avatar-active`
- */
-export const State: Story = {
-  args: {
-    base: 'avatar',
-    content: 'text',
-    size: 'avatar-md',
-    color: 'avatar-primary',
-  },
-  render: args => `
-    <div class="flex gap-4">
-      ${createAvatar(args)}
-      ${createAvatar({ ...args, state: 'avatar-active' })}
-    </div>
-  `,
-};
-
-/**
- * You can override the default styles by adding your own classes with Tailwind CSS.
- */
-export const Custom: Story = {
-  render: () => `
-    <div class="flex gap-4">
-      <div class="avatar avatar-md avatar-primary rounded-none">BK</div>
-      <div class="avatar avatar-md bg-error-pure text-white">BK</div>
-    </div>
-  `,
 };
