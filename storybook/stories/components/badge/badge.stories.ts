6d0bcebf70b50760c44b2ad73acfc848b9147630
João Vitor <joao.oliveira@bkbank.com.br>
2023-02-27 14:51:27 -0300

    fix(badge): migrate to typescript

--- storybook/stories/components/badge/badge.stories.ts
+++ storybook/stories/components/badge/badge.stories.ts
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/html';
+import clsx from 'clsx';
 
 const meta = {
   title: 'Components/Badge',
@@ -6,28 +7,55 @@ const meta = {
     docs: {
       description: {
         component:
-          'The `badge` component is used to represent a notification. Badges should **always have a text content**.<br>Base class: `.badge`. Colors: `.badge-{highlight}`.',
+          'The `badge` component is used to represent a notification. Badges should **always have a text content**.',
       },
     },
   },
   args: {
+    base: 'badge',
+    color: 'badge-highlight',
     content: '32',
   },
   argTypes: {
+    base: {
+      description: 'Base class for the badge.',
+      control: { type: '' },
+      table: { defaultValue: { summary: 'badge' } },
+    },
+    color: {
+      description: 'Available colors for the badge.',
+      table: { defaultValue: { summary: 'Unstyled' } },
+      options: [undefined, 'badge-highlight'],
+      control: {
+        type: 'radio',
+        labels: { undefined: 'Unstyled', 'badge-highlight': 'Highlight' },
+      },
+    },
     content: {
       description: 'Change the content of the badge in the example.',
-      control: {
-        type: 'text',
-      },
+      control: { type: 'text' },
     },
   },
 } satisfies Meta;
 
-type Story = StoryObj<typeof meta>;
-
 export default meta;
 
-export const Default: Story = {
-  render: ({ args }) =>
-    `<div class="badge badge-highlight">${args.content}</div>`,
+export const Default: StoryObj = {
+  render: args =>
+    `<div class="${clsx(args.base, args.color)}">${args.content}</div>`,
+};
+
+/**
+ * Available colors for the badge component.
+ *
+ * Unstyled: `@default`
+ * Highlight: `badge-highlight`
+ */
+export const Colors: StoryObj = {
+  render: args => `
+    <div class="flex gap-4">
+      <div class="${clsx(args.base)}">${args.content}</div>
+      <div class="${clsx(args.base, 'badge-highlight')}">${args.content}</div>
+    </div>
+  `,
 };
