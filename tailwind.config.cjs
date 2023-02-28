30cb5e7bcd89f3aecf9ef2dbae3e0ef52a179923
João Vitor <joao.oliveira@bkbank.com.br>
2023-02-28 09:27:18 -0300

    fix(tailwind): config content path

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./storybook/stories/**/*.{js,jsx,ts,tsx,mdx}'],
+  content: ['./stories/**/*.{js,jsx,ts,tsx,mdx}'],
   theme: {
     extend: { ...require('./dist/core/theme') },
   },
