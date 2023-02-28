2ebceeff2f6daaa12ecc77f4f33175a7703e63e4
João Vitor <joao.oliveira@bkbank.com.br>
2023-02-28 14:48:11 -0300

    docs(radio): remove description

--- stories/components/radio/radio.stories.ts
+++ stories/components/radio/radio.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/html';
-import type { RadioArgs } from './create-radio';
-import { createRadio } from './create-radio';
+import type { RadioArgs } from './Radio';
+import { createRadio } from './Radio';
 
 const meta = {
   title: 'Components/Radio',
@@ -8,8 +8,7 @@ const meta = {
   parameters: {
     docs: {
       description: {
-        component:
-          'To use the radio component, add the `.radio` class to the `input` element.<br>For the label, you can either use a `span` or `label` tag or apply the `.radio-label` class to another element.<br>Base class: `.radio`',
+        component: 'Base class: `radio`',
       },
     },
   },
@@ -31,7 +30,7 @@ type Story = StoryObj<RadioArgs>;
 
 export default meta;
 
-export const Radio: Story = {};
+export const Default: Story = {};
 
 /**
  * To use another element as the radio input label, add the `.radio-label` class to it and the element will inherit the styles.
